874d1e01e8c4c3bae855b44dcf269d503d063cc7
pilarrus <pilar.rus.izquierdo@gmail.com>
2019-11-04 10:23:46 +0100

    captcha

--- tema9/1_captcha.php
+++ tema9/1_captcha.php
@@ -1,17 +1,29 @@
 <?php
-
-    /*Crear imagen a partir de un fondo*/
-    header("Content-Type: image/jpeg");
     $archivo = "fondos/fondo.jpeg";
     $imagen = imagecreatefromjpeg($archivo);
-    $fuente = 5;
-    $x = 75;
-    $y = 35;
+    $tamaño = 25;
+    $min_x = 5;
+    $max_x = 10;
+    $x = random_int($min_x, $max_x);
     $texto = "perro";
-    $color = imagecolorallocate($imagen, 255, 0, 0);
-    $imgText = imagestring($imagen, $fuente, $x, $y, $texto, $color);
+    $longitud_texto = strlen($texto);
+    for ($i=0; $i < $longitud_texto; $i++) {
+        $char = $texto[$i];
+        $angulo = random_int(0, 90);
+        
+        $y = 60;
+        $fuente = realpath("fuentes/OpenSans-Regular.ttf");
+        $color = imagecolorallocate($imagen, 255, 0, 0);
+        //$imgText = imagestring($imagen, $tamaño, $x, $y, $texto, $color); imagefttext
+        $imgText = imagefttext($imagen, $tamaño, $angulo, $x, $y, $color, $fuente, $char);
+        $min_x = $max_x;
+        $max_x = 10*7;
+        $x = random_int($min_x, $max_x);
+    }
+    
+    
+    header("Content-Type: image/jpeg");
     imagejpeg($imagen);
-
     imagedestroy($imagen);
 
 ?>
