5c4ef7fc6b2d6eca1da027c86dc393d9167ce1e3
pilarrus <pilar.rus.izquierdo@gmail.com>
2019-11-11 09:09:16 +0100

    modificado comprobarcookies

--- tema11_cookies_y_json/ejercicio_teorico/comprobarcookies.php
+++ tema11_cookies_y_json/ejercicio_teorico/comprobarcookies.php
@@ -1,8 +1,7 @@
-1 <?php
+<?php
     if(!isset($_COOKIE['Comprobar'])){
-        setcookie('Comprobar',1,time()+3600);
-        header('Refresh: 5;
-        url=http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']);
+        setcookie('Comprobar', 1, time()+3600);
+        header('Refresh: 5; url=http://'. $_SERVER['HTTP_HOST'] . "/" . $_SERVER['PHP_SELF']);
         echo 'Comprobando si tiene activadas las cookies<br/>';
         echo 'Esta comprobaci&oacuten; se reptir&aacute; cada 5 segundos';
     }else{
