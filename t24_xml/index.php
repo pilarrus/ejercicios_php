18b96e2c9282aedec7686ffcd6d580e91bd911ac
pilar <pilar.rus.izquierdo@gmail.com>
2020-02-16 11:16:05 +0100

    Added new node

--- t24_xml/index.php
+++ t24_xml/index.php
@@ -1,55 +1,59 @@
 <?php
 
-include 'catalog.php';
-
 $refBook = [
+    'id' => 'bk103',
     'Author' => 'J.K.Rowling',
-    'Title' => 'Hola',
+    'Title' => 'Harry Potter y la piedra Fisolofal',
     'Genre' => 'Ficción',
     'Price' => '12.99',
     'PublishDate' => '1995-02-01',
     'Description' => 'Un joven mago...'
 ];
 
-function crearNodo($xmlStr, $refBook) {
-    $sxe = new SimpleXMLElement($xmlStr);
-    $book = $sxe->addChild('Book');
-    $book->addAttribute('id', 'bk102');
-
-    foreach ($refBook as $key => $value) {
-        $book->addChild($key, $value);
-    }
-    //echo $sxe->asXML(), '<br/>';
-    //var_dump($sxe);
-    return $sxe->asXML();
-}
-
-$sxe = crearNodo($xmlStr, $refBook);
-//var_dump($sxe);
-
-$xml = simplexml_load_string($sxe);
-//$xml = simplexml_load_string($xmlStr);
-
-$json = json_encode($xml);
-$arrayCatalog = json_decode($json, true);
-
-var_dump($arrayCatalog);
-//echo $arrayCatalog['Book'][0]['@attributes']['id'] . '<br/>';
-/*foreach ($arrayCatalog as $key => $arrayBooks) {
-    //var_dump($key);
-    //var_dump($arrayBooks);
-    foreach ($arrayBooks as $key2 => $arrayBook) {
-        //var_dump($key2);
-        //var_dump($arrayBook);
-        foreach ($arrayBook as $key3 => $value) {
-            //var_dump($key3);
-            //var_dump($value);
-            if(is_array($value)) {
-                //echo $value['id'], '<br/>';
-                if((string) $value['id'] == 'bk101') {
-                    echo "aquí";
+// Copia el xml existente
+function copiarXML($sxe, $array) {
+    foreach ($array as $key => $value) {
+        foreach ($value as $key2 => $value2) {
+            $book = $sxe->addChild($key);
+            //echo $key, '<br>';
+            foreach ($value2 as $key3 => $value3) {
+                if(isset($value3['id'])) {
+                    $id = $value3['id'];
+                    //echo $id, '<br>';
+                    $book->addAttribute('id', $id);
+                } else {
+                    //echo $key3 . " " . $value3 . '<br>';
+                    $book->addChild($key3, $value3);
                 }
             }
         }
     }
-}*/
+    return $sxe;
+}
+
+// Añade un nuevo nodo al xml que he copiado
+function añadirNuevoNodo($sxe, $refBook) {
+    $book = $sxe->addChild('Book');
+
+    foreach ($refBook as $key => $value) {
+        if($key == 'id') {
+            $book->addAttribute($key, $value);
+        } else {
+            $book->addChild($key, $value);
+        }
+    }
+    return $sxe;
+}
+
+if (file_exists('catalog.xml')) {
+    $xmlFile = simplexml_load_file('catalog.xml');
+    $json = json_encode($xmlFile);
+    $array = json_decode($json, true);
+} else {
+    exit('Error al abrir test.xml');
+}
+
+$sxe = new SimpleXMLElement('<Catalog></Catalog>');
+$sxe = copiarXML($sxe, $array);
+$sxe = añadirNuevoNodo($sxe, $refBook);
+$sxe->asXML('catalog2.xml');
